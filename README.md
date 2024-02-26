9cf648c49713b58f772b8efe22bce1db6f0e4af1
mirai-toto <85745033+mirai-toto@users.noreply.github.com>
2024-02-26 15:06:59 +0100

    Quick Squash script

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Simple-Quick-Squash
+# Git Quick Squash Script README
+
+## Description
+
+This bash script allows users to quickly squash the last n number of commits in a git repository. It's handy for cleaning up commit history before pushing to a remote branch. It also bundles commit messages into the new squashed commit message.
+
+<em>Please note this script should be used with caution, as squashing commits will rewrite your commit history.</em>
+
+## Usage
+
+```bash
+./quick-squash.sh [<commit_number>]
+```
+
+## Parameters
+
+- `<commit_number>`: (Optional) The number of recent commits to squash from HEAD. Defaults to 2 if not supplied.
+
+## Features
+
+1. Squashes the last n number of commits together (defaults to 2 if no argument is supplied).
+2. Bundles the commit messages of the squashed commits together in the squashed commit.
+3. Error checks for invalid use cases such as being run outside of a Git repository and squashing when there are fewer than 2 commits in the repository.
+
+## Errors
+
+The script halts with an error message when:
+- The number of commits to squash is less than 2.
+- The argument passed is not an integer.
+- It is run outside of a Git repository.
+- There are fewer than 2 commits in the repository.
+
+## Requirements
+
+This script is written in bash and therefore requires a Unix-like operating system to run. 
+
+Additionally, it requires Git to be installed and accessible via the command line, as it utilizes various Git commands.
+
+## Disclaimer
+
+This script modifies your Git commit history. Always double-check your branch status and ensure you've saved any work before running this script. 
+
+## License
+
+This project is licensed under the terms of the MIT license.
---
+++ quick-squash.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+declare COMMIT_NUMBER
+
+function usage() {
+    if [ "$#" -gt 1 ]; then
+        echo "Error: This script accepts either no argument or one argument - the commit number."
+        echo "Usage: $0 [<commit_number>]"
+        echo "  [<commit_number>] The number of commits to squash from the HEAD. Defaults to 2 if not supplied."
+        exit 1
+    fi
+}
+
+function getCommitNumber() {
+    if [ "$#" -eq 1 ]; then
+        COMMIT_NUMBER=$1
+        if ! [[ $COMMIT_NUMBER =~ ^[0-9]+$ ]]; then
+            echo "Error: Invalid commit_number parameter. It should be an integer."
+            return 1
+        fi
+        if [ "$COMMIT_NUMBER" -lt 2 ]; then
+            echo "Error: Commit_number parameter should be an integer greater than or equal to 2."
+            return 1
+        fi
+    fi
+    return 0
+}
+
+function checkError() {
+    if [ "$#" -eq 0 ]; then
+        COMMIT_NUMBER=2
+        return 1
+    else
+        if ! getCommitNumber "$@"; then
+            return 1
+        fi
+    fi
+
+    if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+        echo "Error: This script must be run inside a Git repository."
+        return 1
+    fi
+
+    if [ "$(git log -$COMMIT_NUMBER --pretty=%B | wc -l)" -lt 4 ]; then
+        echo "Error: There are less than two commits in this repository."
+        return 1
+    fi
+    return 0
+}
+
+function getCommitMessage() {
+    local _commit_number=$1
+    git log --pretty=format:"BEGIN_HASH%h%n%B" | awk -v var="$_commit_number" '/^BEGIN_HASH/ {i++} i==var' | sed '1d;$d'
+}
+
+function quickSquash() {
+    local _commit_number=$1
+    local _commit_message=""
+
+    _commit_message="$(getCommitMessage "$_commit_number")"
+    git reset --soft HEAD~"$_commit_message"
+    git commit -m "$_commit_message"
+    echo "The last two commits have been squashed successfully!"
+}
+
+function main() {
+    if ! checkError "$@"; then
+        echo "err"
+        return 1
+    fi
+    quickSquash "$COMMIT_NUMBER"
+    return 0
+}
+
+main "$@"
